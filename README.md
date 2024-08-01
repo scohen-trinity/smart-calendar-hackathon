a30e5e06a3383f4213a8cb7f4df73bea8f6a6c0c
scohen-trinity <scohen@trinity.edu>
2024-07-31 19:42:43 -0500

    feat: first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+How to get started:
+
+1. Clone the repo: git clone https://github.com/scohen-trinity/smart-calendar-hackathon.git
+2. Run the Server first: `cd server` then `npm start`
+3. Make another terminal for the client
+4. Run the Client: `cd client` then `npm start`
+5. Make a branch for the feature you are working on and name appropriately
+6. Cook
+
+Steps for committing to master
+
+1. Checkout to master
+2. `git pull` to fetch latest changes
+3. Checkout to your feature branch
+4. `git rebase master` to rebase your branch onto master
+5. resolve changes
+6. push changes
+7. create a PR
+8. have someone review (optional depending on time)
+9. merge!
---
+++ server/index.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors"
+import users from "./users.js";
+
+const app = express()
+
+app.use(cors({
+    origin: 'http://localhost:3000', // Specify allowed origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
+    credentials: true // Enable if you need to allow cookies
+}));
+
+app.get("/", (req, res) => {
+    res.send("Jarvis, is this thing on?");
+})
+
+app.get("/api/user", (req, res) => {
+    console.log(users)
+    res.json(users);
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Serve at http://localhost:${port}`);
+})
